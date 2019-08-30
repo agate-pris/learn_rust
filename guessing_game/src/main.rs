0361e452f66df190014a3c2889896208562fbda1
agate-pris <agate-pris@users.noreply.github.com>
2019-08-30 17:01:30 +0900

    wip

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,11 +1,38 @@
+extern crate rand;
+
+//use rand::prelude::*;
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    let size = std::io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}size is {}", guess, size);
-    let bar = rand::random::<u8>();
-    println!("{}", bar);
+    let gen = || rand::thread_rng().gen_range(1, 10);
+    loop {
+        let mine = gen();
+        println!("mine: {}", mine);
+        println!("Expect whether yours is higher or lower than mine.");
+        let mut s = String::new();
+        let size = std::io::stdin().read_line(&mut s).expect("死");
+        let yours = gen();
+        println!("yours: {}", yours);
+        match ("higher".starts_with(s.trim()), "lower".starts_with(s.trim()), yours > mine, yours < mine) {
+            (true, false, true, false) | (false, true, false, true) => {
+                println!("You win!");
+                return;
+            },
+            (true, false, false, true) | (false, true, true, false) => {
+                println!("You lose.");
+                return;
+            },
+            (true, false, false, false) | (false, true, false, false) => {
+                println!("Draw");
+            }
+            (false, false, ..) => {
+                println!("Invalid input {}.", s);
+            },
+            (true, true, ..) | (.., true, true) => {
+                // bug
+            }
+            _ => {},
+        }
+    }
 }
